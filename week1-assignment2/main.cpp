1ea4f07b9693e0c01aea0b493ca04d96e83f500e
Joram Sjamaar <47634296+joram-sjamaar@users.noreply.github.com>
2021-09-16 16:34:24 +0200

    more

--- week1-assignment2/main.cpp
+++ week1-assignment2/main.cpp
@@ -1,22 +1,66 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
+int numbers[] = {
+ 88, 36, 55, 4, 33,
+ 97, 19, 30, 21, 87,
+ 86, 54, 38, 16, 91,
+ 15, 98, 92, 28, 17
+};
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+template <typename T>
+T& calcMax(T *array, size_t length) {
+    size_t max = 0;
 
+    for (size_t i = 0; i < length; i++) {
+        if (array[i] > array[max])
+        {
+            max = i;
+        }
+    }
+
+    return array[max];
+}
+
+template <typename T>
+T& calcMin(T *array, size_t length) {
+    size_t min = 0;
+
+    for (size_t i = 0; i < length; i++) {
+        if (array[i] < array[min])
+        {
+            min = i;
+        }
+    }
+
+    return array[min];
+}
+
+float calcAvg(int *array, size_t length) {
+    float total = 0;
+
+    for (size_t i = 0; i < length; i++) {
+        total += array[i];
+    }
+
+    return total / length;
+}
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+
+    printf("Starting\n\r");
+
+    float min = calcMin(numbers, sizeof(numbers) / sizeof(int));
+    float max = calcMax(numbers, sizeof(numbers) / sizeof(int));
+    float avg = calcAvg(numbers, sizeof(numbers) / sizeof(float));
 
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+
+        printf("Min: %.1f\n\r", min);
+        printf("Max: %.1f\n\r", max);
+        printf("Avg: %.2f\n\r", avg);
+        printf("------------\n\r\n\r");
+        
+        ThisThread::sleep_for(5000ms);
     }
 }
