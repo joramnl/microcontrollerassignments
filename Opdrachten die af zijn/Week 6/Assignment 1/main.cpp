f9b3aa18b4ae20eec003b3e4fdadf3dfd9b8f382
Rick <rickv2014@gmail.com>
2022-01-23 22:15:34 +0100

    6.1 alvast naar af

---
+++ Opdrachten die af zijn/Week 6/Assignment 1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "firstname.h"
+#include "lastname.h"
+
+int main()
+{
+    std::cout << generate_firstname() << " " << generate_lastname() << std::endl;
+    return 0;
+}
